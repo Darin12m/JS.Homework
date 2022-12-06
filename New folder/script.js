df8b750dc38dc126ca2919778dee4ef9fb19535b
Darin12m <119941008+Darin12m@users.noreply.github.com>
2022-12-06 20:14:49 +0100

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,9 +0,0 @@
-let allPhones = 30;
-let phonePrice = 119.95;
-let taxRatePercent = 5;
-
-let onePhonePrice = phonePrice * (1 + (taxRatePercent / 100))
-console.log(onePhonePrice)
-
-let totalPhoneNumber = (onePhonePrice * allPhones);
-console.log(totalPhoneNumber);
